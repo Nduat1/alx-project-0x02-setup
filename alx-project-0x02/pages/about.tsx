3491dcc190a8a479b3fa58916f3a0343ee770a3f
Cate Nduati <nduaticate001@gmail.com>
2025-06-08 23:19:51 +0300

    Button component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,3 +1,15 @@
+import Header from '../components/layout/Header';
+import Button from '../components/common/Button';
+
 export default function AboutPage() {
-  return <h1 className="text-xl">About Us</h1>;
+  return (
+    <>
+      <Header />
+      <div className="space-x-4 p-4">
+        <Button size="small" shape="rounded-sm">Small</Button>
+        <Button size="medium" shape="rounded-md">Medium</Button>
+        <Button size="large" shape="rounded-full">Large</Button>
+      </div>
+    </>
+  );
 }
