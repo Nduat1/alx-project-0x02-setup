2401039a613066ecc09b24364c5fbb30ece52bb5
Cate Nduati <nduaticate001@gmail.com>
2025-06-08 19:25:42 +0300

    Card component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,12 +1,13 @@
-import {type CardProps } from "@/interfaces";
+import React from 'react';
+import { type CardProps } from '@/interfaces';
 
-const Card: React.FC<CardProps> = ({title, content}) => {
-    return(
-        <div className="border p-4 rounded shadow-md mb-4">
-            <h2 className="text-xl font-bold">{title}</h2>
-            <p>{content}</p>
-        </div>
-    )
-}
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div className="p-4 bg-white rounded shadow-md mb-4">
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
