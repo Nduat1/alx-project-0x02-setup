7160758199112177adf0858e5c8531b4fea8204f
Cate Nduati <nduaticate001@gmail.com>
2025-06-08 18:17:05 +0300

    Basic routing

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -1,8 +1,10 @@
-const Header: React.FC = () => {
-    return(
-        <>
-        </>
-    )
-} 
+import Link from 'next/link';
 
-export default Header;
+export default function Header() {
+  return (
+    <nav className="flex space-x-4 p-4 bg-gray-200">
+      <Link href="/home">Home</Link>
+      <Link href="/about">About</Link>
+    </nav>
+  );
+}
---
+++ alx-project-2/pages/home.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1 className="text-xl">This is the Home Page</h1>;
+}
